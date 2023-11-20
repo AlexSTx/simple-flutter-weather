3de523ce9b25b590058014a7e0c6dd147f75df6b
ale-stx <alexsanderst@id.uff.br>
2023-11-20 11:56:28 -0300

    feat: WeatherForecast objects created from api response

--- lib/controllers/weather_controller.dart
+++ lib/controllers/weather_controller.dart
@@ -1,12 +1,62 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> fetch5DayWeather(double lat, double lon) async {
+class WeatherForecast {
+  final DateTime? dateTime;
+  final double temp;
+  final double feelsLike;
+  final double tempMin;
+  final double tempMax;
+  final double pressure;
+  final double humidity;
+  final String mainWeatherStatus;
+  final String description;
+  final String icon;
+
+  WeatherForecast({
+    required this.dateTime,
+    required this.mainWeatherStatus,
+    required this.description,
+    required this.icon,
+    required this.temp,
+    required this.feelsLike,
+    required this.tempMin,
+    required this.tempMax,
+    required this.pressure,
+    required this.humidity,
+  });
+
+  factory WeatherForecast.fromJson(Map<String, dynamic> json) {
+    return WeatherForecast(
+      dateTime: DateTime.fromMillisecondsSinceEpoch((json['dt'] as int) * 1000),
+      temp: json['main']['temp'] as double,
+      feelsLike: json['main']['feels_like'] as double,
+      tempMin: json['main']['temp_min'] as double,
+      tempMax: json['main']['temp_max'] as double,
+      pressure: json['main']['pressure'] as double,
+      humidity: json['main']['humidity'] as double,
+      mainWeatherStatus: json['weather'][0]['main'] as String,
+      description: json['weather'][0]['description'] as String,
+      icon: json['weather'][0]['icon'] as String,
+    );
+  }
+}
+
+Future<List<WeatherForecast>> fetch5DayWeather(double lat, double lon) async {
   final response = await http.get(Uri.parse(
       'https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&units=metric&appid=${dotenv.env['API_KEY']}'));
 
   if (response.statusCode == 200) {
-    print(response.body);
+    var decodedResponse = jsonDecode(response.body);
+    var forecasts = <WeatherForecast>[];
+
+    for (var result in decodedResponse['list']) {
+      forecasts.add(WeatherForecast.fromJson(result));
+    }
+    
+    return forecasts;
   } else {
     throw Exception('Failed to fetch data for coordinates given');
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,19 @@ import 'package:weather_app/screens/search_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'controllers/geocoding_controller.dart';
+import 'controllers/weather_controller.dart';
 
 Future main() async {
   await dotenv.load(fileName: '.env');
+
+  var pos = await fetchPositions('Rio de Janeiro');
+
+  try {
+    var temp = await fetch5DayWeather(pos[0].lat, pos[0].lon);
+    print(temp);
+  } catch (err) {
+    print(err);
+  }
 
   runApp(WeatherApp());
 }
